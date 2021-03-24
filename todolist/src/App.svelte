d618c0a0891d3323c711b13c56d56b3905093505
Jabelic <hibiki990523@gmail.com>
2021-03-25 02:25:22 +0900

    todo list:add,  toggle, filter

--- todolist/src/App.svelte
+++ todolist/src/App.svelte
@@ -1,5 +1,27 @@
 <script>
 	export let name;
+	export let todoList = [
+		{id: 0, done: false, title: '定期券の購入'},
+		{id: 1, done: false, title: 'トイレットペーパーの購入'},
+	]
+	export let currentText = ''
+	export const addTask = () => {
+		console.debug(todoList)
+		if(currentText){
+			todoList = [...todoList,
+			{
+				id: todoList.length,
+				done: false,
+				title: currentText
+			}
+		]
+		currentText = ''
+		}
+	}
+	let condition = null
+	$: filterTodoList = () => {
+		return condition === null ? todoList : todoList.filter(todo=> todo.done === condition)
+	}
 </script>
 
 <main>
@@ -8,9 +30,25 @@
 	<h2>TODO List </h2>
 	<div>
 		絞り込み: 
-		<button>All</button>
-		<button>Undone</button>
-		<button>Done</button>
+		<button on:click={()=>{condition = null}} >All</button>
+		<button on:click={()=>{condition = false}}>Undone</button>
+		<button on:click={()=>{condition = true}}>Done</button>
+	</div>
+	<div>
+		<input type="text" bind:value={currentText}>
+		<button on:click={addTask}>Submit</button>
+	</div>
+	<div>
+		<ul>
+			<!-- {#each todoList as todo (todo.id)} -->
+			{#each filterTodoList() as todo (todo.id)}
+			{#key todo.done}
+			<li>
+				<input type="checkbox" bind:checked={todo.done}>{todo.title}
+			</li>
+			{/key}
+			{/each}
+		</ul>
 	</div>
 
 </main>
